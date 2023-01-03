ba119c9824264cb1a46f4348b9abe239a12807a1
Thomas Verderesi <tverderesi@gmail.com>
2023-01-03 16:53:49 -0300

    split index into typeDefs and resolvers

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
-const gql = require('graphql-tag');
 
 const { MONGODB } = require('./config');
-const Post = require('./modules/Post');
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-  },
-};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
