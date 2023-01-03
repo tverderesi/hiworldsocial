da84661e2ac87b73cf3c0b0ca3ced03e72853299
Thomas Verderesi <tverderesi@gmail.com>
2023-01-03 16:51:25 -0300

    Added Post Types and getPost

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
 const gql = require('graphql-tag');
+
+const { MONGODB } = require('./config');
+const Post = require('./modules/Post');
+
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
-    # ! means required
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello World',
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
-const server = new ApolloServer(typeDefs, resolvers);
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen(
-  { port: 5000 }.then(res => {
-    console.log(`Server running at ${res.URL}`);
-  })
-);
+mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
+  console.log('MongoDB connected.');
+  server.listen({ port: 5000 }).then(res => {
+    console.log(`Server running at ${res.url}`);
+  });
+});
