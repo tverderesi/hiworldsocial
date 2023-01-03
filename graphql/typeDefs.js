58448927da7e853a40dd8344a6446c8670c1339c
Thomas Verderesi <tverderesi@gmail.com>
2023-01-03 17:19:37 -0300

    Added input RegisterInput, type User and Mutations

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,7 +7,29 @@ const typeDefs = gql`
     createdAt: String!
     username: String!
   }
+
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+
   type Query {
     getPosts: [Post]
   }
+
+  type User {
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+  }
 `;
+
+module.exports = typeDefs;
