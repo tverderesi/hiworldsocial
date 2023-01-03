a0991ccd8649a60ec712bfbabb57156442f63d41
Thomas Verderesi <tverderesi@gmail.com>
2023-01-03 16:53:45 -0300

    split index into typeDefs and resolvers

---
+++ graphql/resolvers.js
@@ -0,0 +1,14 @@
+const Post = require('../modules/Post');
+
+const resolvers = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
