8bac5fff7e2d453fbcbf7a0bc4ffbd9f4e59b109
Thomas Verderesi <tverderesi@gmail.com>
2023-01-03 14:10:44 -0300

    added server test

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const typeDefs = gql`
-type Query = 
+  type Query {
+    sayHi: String!
+    # ! means required
+  }
 `;
+
+const resolvers = {
+  Query: {
+    sayHi: () => 'Hello World',
+  },
+};
+
+const server = new ApolloServer(typeDefs, resolvers);
+
+server.listen(
+  { port: 5000 }.then(res => {
+    console.log(`Server running at ${res.URL}`);
+  })
+);
