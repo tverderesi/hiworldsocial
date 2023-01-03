0c3fed0bff43142e7e4364383992429d72ce9595
Thomas Verderesi <tverderesi@gmail.com>
2023-01-03 17:19:44 -0300

    Updated imports

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const { MONGODB } = require('./config');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
