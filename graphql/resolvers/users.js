178997423710503a4b739a145238989b085ac5d7
Thomas Verderesi <tverderesi@gmail.com>
2023-01-04 12:02:10 -0300

    Added username validation.

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,19 +1,26 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../../config');
-
+const { UserInputError } = require('apollo-server');
 const User = require('../../models/User');
 
 module.exports = {
   Mutation: {
     async register(
       _, //párent argument
-      { registerInput: { username, email, password, confirmPassword } }, //args argument
-      context,
-      info
+      { registerInput: { username, email, password, confirmPassword } } //args argument
     ) {
       //todo validate user data
-      //todo make sure user doesnt already exist
+
+      //make sure user doesnt already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError('Username is taken.', {
+          errors: {
+            username: 'This username is taken!',
+          },
+        });
+      }
 
       //hash password and create an auth token
       password = await bcrypt.hash(password, 12);
