4bea3c2ac6858776d590e8c0cd15d52bb9ffd7a5
Thomas Verderesi <tverderesi@gmail.com>
2023-01-10 20:37:47 -0300

    Added Links to Menu Items.

--- client/src/components/MenuBar.tsx
+++ client/src/components/MenuBar.tsx
@@ -1,9 +1,14 @@
+import path from 'path';
 import { useState } from 'react';
-import { Menu, MenuItemProps, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+import { Menu, MenuItemProps } from 'semantic-ui-react';
 
 export default function MenuBar() {
   const [state, setState] = useState({ activeItem: 'home' });
   const { activeItem } = state;
+  const pathname = window.location.pathname;
+  const path = '/' ? 'home' : pathname.substring(1);
 
   const handleItemClick = (
     e: React.SyntheticEvent<any, any>,
@@ -11,36 +16,36 @@ export default function MenuBar() {
   ) => (name ? setState({ activeItem: name }) : '');
 
   return (
-    <div>
-      <Menu
-        pointing
-        secondary
-      >
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          name='friends'
-          active={activeItem === 'friends'}
-          onClick={handleItemClick}
-        />
-        <Menu.Menu position='right'>
-          <Menu.Item
-            name='logout'
-            active={activeItem === 'logout'}
-            onClick={handleItemClick}
-          />
-        </Menu.Menu>
-      </Menu>
+    <Menu
+      pointing
+      secondary
+      size='massive'
+      color='purple'
+    >
+      <Menu.Item
+        name='home'
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+        as={Link}
+        to='/'
+      />
 
-      <Segment></Segment>
-    </div>
+      <Menu.Menu position='right'>
+        <Menu.Item
+          name='Login'
+          active={activeItem === 'login'}
+          onClick={handleItemClick}
+          as={Link}
+          to='/login'
+        />
+        <Menu.Item
+          name='Register'
+          active={activeItem === 'register'}
+          onClick={handleItemClick}
+          as={Link}
+          to='/register'
+        />
+      </Menu.Menu>
+    </Menu>
   );
 }
