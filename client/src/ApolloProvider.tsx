14b43b2007c46e4157524a3afdf526c50123f514
Thomas Verderesi <tverderesi@gmail.com>
2023-01-16 16:20:21 -0300

    Added context for Authorization

--- client/src/ApolloProvider.tsx
+++ client/src/ApolloProvider.tsx
@@ -4,12 +4,25 @@ import {
   createHttpLink,
   ApolloProvider,
   ApolloClient,
+  ApolloLink,
 } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({ uri: 'http://localhost:5000' });
 
-const client = new ApolloClient({ link: httpLink, cache: new InMemoryCache() });
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: { Authorization: token ? `Bearer ${token}` : '' },
+  };
+});
 
+const client = new ApolloClient({
+  //@ts-ignore
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+console.log(client);
 export default (
   <ApolloProvider client={client}>
     <App />
