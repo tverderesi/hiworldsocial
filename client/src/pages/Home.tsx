9a375af616ec2cb8be4d05b7141ec56d05723c67
Thomas Verderesi <tverderesi@gmail.com>
2023-01-16 16:21:39 -0300

    style and new post/ad inclusion

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,56 +1,46 @@
 import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
-import { Grid } from 'semantic-ui-react';
+import { useContext } from 'react';
+import { Container, Grid } from 'semantic-ui-react';
+import Ad from '../components/Ad';
+import NewPost from '../components/NewPost';
 import Post from '../components/Post';
+import { AuthContext } from '../context/auth';
+import { FETCH_POSTS_QUERY } from '../util/GraphQL';
 
 function Home() {
   const { loading, data } = useQuery(FETCH_POSTS_QUERY); //can't destructure here or else TS will scream
   const posts = data?.getPosts;
+  const { user } = useContext(AuthContext);
+
   return (
-    <>
+    <Container>
       <Grid columns={3}>
-        <Grid.Row>
+        <Grid.Row style={{ marginBottom: '-2rem' }}>
           <h1 className='page-title'>Recent Posts</h1>
         </Grid.Row>
+
         <Grid.Row>
+          <Grid.Column style={{ marginTop: '2rem ' }}>
+            {user ? <NewPost /> : <Ad />}
+          </Grid.Column>
+
           {loading ? (
             <h1>Loading</h1>
           ) : (
             posts &&
             posts.map((post: any) => (
-              <Grid.Column key={post.id}>
+              <Grid.Column
+                key={post.id}
+                style={{ marginTop: '2rem ' }}
+              >
                 <Post post={post} />
               </Grid.Column>
             ))
           )}
         </Grid.Row>
       </Grid>
-    </>
+    </Container>
   );
 }
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      commentCount
-      likeCount
-      comments {
-        id
-        createdAt
-        username
-        body
-      }
-      likes {
-        id
-        createdAt
-        username
-      }
-    }
-  }
-`;
 
 export default Home;
