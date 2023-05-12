97f11239aff121cef927ba3ab9dedde34090bd51
tverderesi <tverderesi@gmail.com>
2023-05-12 15:33:40 -0300

    corrected typo

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,18 +1,18 @@
-import { Button, Form } from 'semantic-ui-react';
-import { useState, useContext } from 'react';
-import { useMutation } from '@apollo/client';
-import { useNavigate } from 'react-router-dom';
+import { Button, Form } from "semantic-ui-react";
+import { useState, useContext } from "react";
+import { useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
 
-import { useForm } from '../util/hooks';
-import { AuthContext } from '../context/auth';
-import { LOGIN_USER } from '../util/GraphQL';
+import { useForm } from "../util/hooks";
+import { AuthContext } from "../context/auth";
+import { LOGIN_USER } from "../util/GraphQL";
 
 export default function Login() {
   const context = useContext(AuthContext);
   const navigate = useNavigate();
   const [errors, setErrors] = useState({}) as any;
 
-  const initialState = { username: '', password: '' };
+  const initialState = { username: "", password: "" };
 
   const { onChange, onSubmit, values } = useForm(
     loginUserCallback,
@@ -22,7 +22,7 @@ export default function Login() {
   const [loginUser, { loading }] = useMutation(LOGIN_USER, {
     update(_, { data: { login: userData } }) {
       context.login(userData);
-      navigate('/', { replace: true });
+      navigate("/", { replace: true });
     },
     onError(err: any) {
       setErrors(err.graphQLErrors[0]?.extensions?.errors);
@@ -37,52 +37,49 @@ export default function Login() {
   return (
     <div
       style={{
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
       }}
     >
       <h1
-        className='page-title'
-        style={{ marginBottom: '2em', marginTop: '1em' }}
+        className="page-title"
+        style={{ marginBottom: "2em", marginTop: "1em" }}
       >
         Login
       </h1>
       <Form
         onSubmit={onSubmit}
-        noValidade
-        style={{ width: '50%' }}
-        className={loading ? 'loading' : ''}
+        noValidate
+        style={{ width: "50%" }}
+        className={loading ? "loading" : ""}
       >
         <Form.Input
-          label='Username'
-          placeholder='Username'
-          name='username'
+          label="Username"
+          placeholder="Username"
+          name="username"
           value={values.username}
           onChange={onChange}
           error={errors?.username ? true : false}
         />
 
         <Form.Input
-          type='password'
-          label='Password'
-          placeholder='Password'
-          name='password'
+          type="password"
+          label="Password"
+          placeholder="Password"
+          name="password"
           value={values.password}
           onChange={onChange}
           error={errors?.password ? true : false}
         />
 
-        <Button
-          type='submit'
-          primary
-        >
+        <Button type="submit" primary>
           login
         </Button>
       </Form>
       {errors && Object.keys(errors).length > 0 && (
-        <div className='ui error message'>
-          <ul className='list'>
+        <div className="ui error message">
+          <ul className="list">
             {Object.values(errors).map((value: any) => (
               <li key={value}>{value}</li>
             ))}
